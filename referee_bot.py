5220b547a32f250aa241353aa788ecf8b8204698
Alexis-Guignot <72448177+Alexis-Guignot@users.noreply.github.com>
2022-04-23 19:02:09 +0200

    Update referee_bot.py

--- referee_bot.py
+++ referee_bot.py
@@ -5,9 +5,8 @@ import os
 
 token = os.environ.get('BOT_TOKEN')
 
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix=".", intents=intents)
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
